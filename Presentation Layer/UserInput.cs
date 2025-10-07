370db5fe6ce3c0973f0dc5591ef883a6c30286db
Robin-LeighBasson-source <robin_leigh.basson@outlook.com>
2025-10-07 19:51:08 +0200

    Surname movement

--- Presentation Layer/UserInput.cs
+++ Presentation Layer/UserInput.cs
@@ -20,9 +20,10 @@ namespace Student_System_Manager.Presentation_Layer
         //Encapsulation
         public string StudentID { get => studentID; set => studentID = value; }
         public string Name { get => name; set => name = value; }
+        public string Surname { get => surname; set => surname = value; }
         public int Age { get => age; set => age = value; }
         public string Course { get => course; set => course = value; }
-        public string Surname { get => surname; set => surname = value; }
+        
 
         //Paramaterised Constructor
         public UserInput(string studentID, string name, string surname, int age, string course)
