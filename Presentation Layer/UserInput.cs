3d99957f81678aabf742fa43b4c18a41954f492b
Robin-LeighBasson-source <robin_leigh.basson@outlook.com>
2025-10-07 10:34:12 +0200

    Fields Added

--- Presentation Layer/UserInput.cs
+++ Presentation Layer/UserInput.cs
@@ -8,5 +8,18 @@ namespace Student_System_Manager.Presentation_Layer
 {
     internal class UserInput
     {
+        //Fields
+        string name;
+        string studentID;
+        int age;
+        string course;
+
+        //Encapsulation
+        public string Name { get => name; set => name = value; }
+        public string StudentID { get => studentID; set => studentID = value; }
+        public int Age { get => age; set => age = value; }
+        public string Course { get => course; set => course = value; }
+
+        //
     }
 }
